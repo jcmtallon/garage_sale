2406d98f3dd76237ef03da624137dbee940927ea
trueStoryJapan <juanca_mt@hotmail.com>
2021-07-14 18:29:19 +0900

    feat: add temp good data fetch and display

--- src/pages/api/goods/index.ts
+++ src/pages/api/goods/index.ts
@@ -4,8 +4,7 @@ import { getGoods, addGood } from "../../../services/googleSheets/sheets";
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === "GET") {
     const goods = await getGoods();
-    res.statusCode = 200;
-    res.json(goods);
+    res.status(200).json(goods);
   }
   if (req.method === "POST") {
     const response = await addGood();
