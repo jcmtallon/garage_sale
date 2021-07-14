40c31c12771149b9c3d0e068a7e168d83b4a815d
trueStoryJapan <juanca_mt@hotmail.com>
2021-07-14 17:57:08 +0900

    feat: temp post testing button

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
+  const postGood = async () => {
+    await fetch("/api/goods", {
+      method: "POST",
+    });
+  };
+
   return (
     <div className={styles.container}>
       <Head>
@@ -17,7 +23,7 @@ export default function Home() {
         </h1>
 
         <p className={styles.description}>
-          Get started by editing{' '}
+          Get started by editing{" "}
           <code className={styles.code}>pages/index.js</code>
         </p>
 
@@ -58,12 +64,13 @@ export default function Home() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Powered by{' '}
+          Powered by{" "}
           <span className={styles.logo}>
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
           </span>
         </a>
       </footer>
+      <button onClick={postGood}>TEST POST</button>
     </div>
-  )
+  );
 }
