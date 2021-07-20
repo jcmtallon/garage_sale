d9c56f40ba3a561ef3115f0f4f185baa3cb5fe40
trueStoryJapan <juanca_mt@hotmail.com>
2021-07-20 11:58:21 +0900

    refactor: simplify logo display

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,29 +1,13 @@
-import React, { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
-import { useMdScreenQuery } from "../hooks/useMediaQuery";
 import { SecondaryButton } from "./elements/SecondaryButton";
-
-const url = {
-  regular: "logo.svg",
-  md: "logo_md.svg",
-};
 
 interface OwnProps {
   selected: number;
 }
 
 export const TopBar = ({ selected = 1 }: OwnProps) => {
-  const [logoUrl, setLogoUrl] = useState(url.regular);
-
   const { t } = useTranslation();
-  const isMdScreen = useMdScreenQuery();
-
-  //Strategy used to prevent 'Prop src did not match between client and server' error.
-  // Reference: https://github.com/vercel/next.js/issues/10608
-  useEffect(() => {
-    setLogoUrl(isMdScreen ? url.md : url.regular);
-  }, [isMdScreen]);
 
   const onBookButtonClick = () => {
     //TODO: click handling.
@@ -34,7 +18,20 @@ export const TopBar = ({ selected = 1 }: OwnProps) => {
     <div className="bg-primary-600 fixed w-full h-12 md:h-14">
       <div className="px-2 md:px-6 max-w-screen-xl m-auto h-full">
         <div className="flex justify-between items-center h-full">
-          <img src={logoUrl} alt="Garage Sale Logo" />
+          {/* Desktop logo */}
+          <img
+            src={"logo_md.svg"}
+            alt="Garage Sale Logo"
+            className="hidden md:block"
+          />
+
+          {/* Mobile logo */}
+          <img
+            src={"logo.svg"}
+            alt="Garage Sale Logo"
+            className="block md:hidden"
+          />
+
           <div className="flex">
             <div>TODO: Language selector</div>
             <div>TODO: Selected Items counter</div>
--- src/hooks/useMediaQuery.ts
+++
@@ -1,8 +0,0 @@
-import { useMediaPredicate } from "react-media-hook";
-
-const mdScreenPredicate = "(min-width: 768px)";
-
-export const useMdScreenQuery = () => {
-  const isMd = useMediaPredicate(mdScreenPredicate);
-  return isMd;
-};
