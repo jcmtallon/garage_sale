e1c4871ca346b6d0dd5a3be7a5f52ec4f4d10228
trueStoryJapan <juanca_mt@hotmail.com>
2021-07-27 21:19:07 +0900

    feat: testing img attribute

--- src/components/pages/home/GoodCard/HomeGoodCardThumbnail.tsx
+++ src/components/pages/home/GoodCard/HomeGoodCardThumbnail.tsx
@@ -1,18 +1,16 @@
-import Image from "next/image";
-
 interface OwnProps {
   imageId: string;
 }
 
 export const HomeGoodCardThumbnail = ({ imageId }: OwnProps) => {
   return (
-    <div className="relative w-full h-full">
-      <Image
+    <div className="relative w-full h-full object-cover">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
         src={`https://drive.google.com/uc?export=view&id=${imageId}`}
-        layout="fill"
-        objectFit="cover"
         alt="Item thumbnail"
         draggable={false}
+        loading="lazy"
         className="rounded bg-gray-50 shadow-inner cursor-pointer select-none"
       />
     </div>
