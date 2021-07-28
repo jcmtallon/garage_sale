b5685d71287c322629e289e02cd1e9653d9ec06a
trueStoryJapan <juanca_mt@hotmail.com>
2021-07-29 00:39:37 +0900

    feat: remove category

--- src/constants/goodCategoryOptions.ts
+++ src/constants/goodCategoryOptions.ts
@@ -3,7 +3,6 @@ import { GoodCategory, GOOD_CATEGORY } from "./goodCategory";
 
 export const GOOD_CATEGORY_OPTIONS: ConstantValueOptions<GoodCategory> = [
   { value: GOOD_CATEGORY.APPLIANCES, labelKey: "goodCategory.appliances" },
-  { value: GOOD_CATEGORY.BATHROOM, labelKey: "goodCategory.bathroom" },
   { value: GOOD_CATEGORY.GAMES, labelKey: "goodCategory.games" },
   { value: GOOD_CATEGORY.FASHION, labelKey: "goodCategory.fashion" },
   { value: GOOD_CATEGORY.FURNITURE, labelKey: "goodCategory.furniture" },
