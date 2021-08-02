892ca214e14665b6038284d684cce18e38ac54f7
trueStoryJapan <juanca_mt@hotmail.com>
2021-08-03 01:22:38 +0900

    feat: remove conflicting inset css from img element

--- src/components/pages/home/GoodCard/HomeGoodCardThumbnail.tsx
+++ src/components/pages/home/GoodCard/HomeGoodCardThumbnail.tsx
@@ -12,22 +12,15 @@ export const HomeGoodCardThumbnail = ({
 }: OwnProps) => {
   return (
     <div className="relative w-full h-full">
-      <div
-        className="block overflow-hidden absolute box-border m-0"
-        style={{ inset: "0px" }}
-      >
-        <img
-          src={imageUrl}
-          alt="Item thumbnail"
-          draggable={false}
-          decoding="async"
-          sizes="100vw"
-          style={{ inset: "0px" }}
-          className={`rounded bg-gray-50 shadow-inner cursor-pointer select-none object-cover absolute box-border p-0 border-none m-auto block w-0 h-0 min-w-full max-w-full min-h-full max-h-full ${
-            isGiven ? "filter grayscale" : ""
-          }`}
-        />
-      </div>
+      <img
+        src={imageUrl}
+        alt="Item thumbnail"
+        draggable={false}
+        decoding="async"
+        className={`rounded bg-gray-50 shadow-inner cursor-pointer select-none object-cover absolute box-border p-0 border-none m-auto block w-0 h-0 min-w-full max-w-full min-h-full max-h-full ${
+          isGiven ? "filter grayscale" : ""
+        }`}
+      />
     </div>
   );
 };
