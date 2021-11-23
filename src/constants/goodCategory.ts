7bfdcc3ebd9dded6e86da16ca971cf306bbcd521
jcmtallon <juanca_mt@hotmail.com>
2021-11-24 00:25:16 +0100

    build: install ts-utils + sample

--- src/constants/goodCategory.ts
+++ src/constants/goodCategory.ts
@@ -1,13 +1,15 @@
-export const GOOD_CATEGORY = {
-  APPLIANCES: "APPLIANCES",
-  GAMES: "GAMES",
-  FASHION: "FASHION",
-  FURNITURE: "FURNITURE",
-  HOME: "HOME",
-  KITCHEN: "KITCHEN",
-  LIGHTING: "LIGHTING",
-  OTHER: "OTHER",
-  TOOLS: "TOOLS",
-} as const;
+import { Constant, createConstant } from '@jcmtallon/ts-utils'
 
-export type GoodCategory = typeof GOOD_CATEGORY[keyof typeof GOOD_CATEGORY];
+export const GOOD_CATEGORY = createConstant([
+  'APPLIANCES',
+  'GAMES',
+  'FASHION',
+  'FURNITURE',
+  'HOME',
+  'KITCHEN',
+  'LIGHTING',
+  'OTHER',
+  'TOOLS',
+])
+
+export type GoodCategory = Constant<typeof GOOD_CATEGORY>
